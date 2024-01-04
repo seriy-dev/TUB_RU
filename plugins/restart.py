8bd659e920464043c1a2acb5bb92e5ab3b196e33
Fimkov <123477029+Timka4959000@users.noreply.github.com>
2024-01-04 21:07:32 +0700

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -8,7 +8,7 @@ prefix = get_prefix()
 @Client.on_message(filters.command("restart", prefix) & filters.me)
 async def restart_userbot(_, message):
     await message.edit("<b>Перезагружаю юзербота...</b>")
-    await restart(message.chat.id)
+    await restart(message)
 
 add_module("restart", __file__)
 add_command("restart", f"{prefix}restart", "перезапускает юзербота")
