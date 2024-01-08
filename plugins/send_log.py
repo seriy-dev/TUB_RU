b19c5c1a0ddaf0ea8329c1d2c57becb40fd66c1c
Fimkov <123477029+Timka4959000@users.noreply.github.com>
2024-01-08 16:57:50 +0700

    Update send_log.py

--- plugins/send_log.py
+++ plugins/send_log.py
@@ -1,19 +1,41 @@
 from pyrogram import Client, filters
 from helps.modules import add_module, add_command
-
+from helps.scripts import get_lang
 from helps.get_prefix import get_prefix
+
+lang = get_lang()
 prefix = get_prefix()
+
+class Texts:
+    @staticmethod
+    def get_texts():
+        return {"logs": {
+            "ru": f"<b>Логи TimkaUserBot</b>",
+            "en": f"<b>Logs TimkaUserBot</b>"
+        },
+            "error": {
+                "ru": "Лог пустой!",
+                "en": "The log is empty!"
+            }
+        }
+
 
 @Client.on_message(filters.command("send_log", prefixes=prefix) & filters.me)
 async def send_log(client, message):
+    text_versions = Texts.get_texts()
     try:
         await client.send_document(
             chat_id=message.chat.id,
             document=open("log.log", "rb"),
-            caption="<b>Logs TimkaUserBot</b>")
+            caption=text_versions["logs"][lang)
         await message.delete()
     except:
-        await message.edit("Лог пустой!")
+        await message.edit(text_versions['error'][lang])
 
-add_module("send_log", __file__)
-add_command("send_log", f"{prefix}send_log", "Отправляет файл с логами")
+
+if lang == "ru":
+    add_module("send_log", __file__)
+    add_command("send_log", f"{prefix}send_log", "Отправляет файл с логами")
+else:
+    add_module("send_log", __file__)
+    add_command("send_log", f"{prefix}send_log", "Sends a file with logs")
