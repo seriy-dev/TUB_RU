2f13d1421b1926093889f0d66eaeade0a5feb85d
Fimkov <123477029+Timka4959000@users.noreply.github.com>
2024-01-08 16:56:47 +0700

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,8 +1,53 @@
 from pyrogram import Client, filters
 from helps.modules import add_module, add_command, modules, commands, get_file_by_module
-
+from helps.scripts import get_lang
 from helps.get_prefix import get_prefix
+
+lang = get_lang()
 prefix = get_prefix()
+
+class Texts:
+    @staticmethod
+    def get_texts(name=None, file=None, commands=None, all_modules=None):
+        return {"error": {
+            "ru": f"Данный модуль не найден",
+            "en": f"This module was not found"
+        },
+            "module": {
+                "ru": f"""
+📔 Название модуля: <code>{name}</code>
+📁 Расположение модуля: <code>{file}</code>
+
+• Команды модуля:
+<i>{commands}</i>
+                """,
+                "en": f"""
+📔 Module name: <code>{name}</code>
+📁 Module location: <code>{file}</code>
+
+• Module commands:
+<i>{commands}</i>
+                """
+            },
+            "modules": {
+                "ru": f"""
+<b>📔 Все модули
+         TimkaUserBot:</b>
+{all_modules}
+
+ℹ Всего модулей: {len(modules)}
+❗ Для получении информации о модуле: {prefix}help [модуль]
+                """,
+                "en": f"""
+<b>📔 All modules
+         TimkaUserBot:</b>
+{all_modules}
+
+ℹ Total modules: {len(modules)}
+❗ To get information about the module: {prefix}help [module]
+                """
+            }
+        }
 
 @Client.on_message(filters.command("help", prefixes=prefix) & filters.me)
 async def show_help(client, message):
@@ -14,30 +59,24 @@ async def show_help(client, message):
             for command_name, help_command in module_commands.items():
                 result = result + f"{command_name} - {help_command}\n"
             file = get_file_by_module(args[1])
-            await message.edit(f"""
-📔 Название модуля: {args[1]}
-📁 Расположение модуля: {file}
-
-• Команды модуля:
-
-{result}
-                """)
+            text_versions = Texts.get_texts(name=args[0], file=file, commands=result)
+            await message.edit(text_versions['module'][lang])
         else:
-            await message.edit("Данный модуль не найден")
+            text_versions = Texts.get_texts()
+            await message.edit(text_versions['error'][lang])
     except IndexError:
         all_modules = ""
         for module in modules:
             all_modules = all_modules + f"⟩⟩ <code>{module}</code>\n"
-        await message.edit(f"""
-<b>📔 Все модули
-         TimkaUserBot:</b>
-{all_modules}
-
-ℹ Всего модулей: {len(modules)}
-❗ Смотреть все команды модулей и подробное разъяснение: {prefix}help [модуль]
-                """)
+        text_versions = Texts.get_texts(all_modules=all_modules)
+        await message.edit(text_versions['modules'][lang])
         return
 
-add_module("help", __file__)
-add_command("help", f"{prefix}help", "увидеть все модули")
-add_command("help", f"{prefix}help [модуль]", "посмотреть описание модуля")
+if lang == "ru":
+    add_module("help", __file__)
+    add_command("help", f"{prefix}help", "Просмотреть все модули")
+    add_command("help", f"{prefix}help [имя модуля]", "Посмотреть описание модуля и команды")
+else:
+    add_module("help", __file__)
+    add_command("help", f"{prefix}help", "View all modules")
+    add_command("help", f"{prefix}help [module name]", "View module description and commands")
