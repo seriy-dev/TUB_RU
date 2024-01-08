f5908d27db409e9311bc073ee769bbffb7f7c246
Fimkov <123477029+Timka4959000@users.noreply.github.com>
2024-01-08 16:57:18 +0700

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,18 +1,36 @@
 from datetime import datetime
 from pyrogram import Client, filters
 from helps.modules import add_module, add_command
-
+from helps.scripts import get_lang
 from helps.get_prefix import get_prefix
+
 prefix = get_prefix()
+lang = get_lang()
+
+
+class Texts:
+    @staticmethod
+    def get_texts():
+        return {"ping": {
+            "ru": f"<i>Понг!</i>\n<b>🏓 Пинг равен ",
+            "en": f"<i>Pong!</i>\n<b>🏓 Ping is equal "
+        }
+        }
 
 
 @Client.on_message(filters.command("ping", prefixes=prefix) & filters.me)
 async def ping(client, message):
+    text_versions = Texts.get_texts()
     start_t = datetime.now()
     await message.edit("🏓")
     end_t = datetime.now()
     time_taken_s = (end_t - start_t).microseconds / 1000
-    await message.edit(f"<i>Понг!</i>\n<b>🏓 Пинг равен {round(time_taken_s)}ms</b>")
+    await message.edit(text_versions['ping'][lang] + f"{round(time_taken_s)}ms</b>")
 
-add_module("ping", __file__)
-add_command("ping", f"{prefix}ping", "получить пинг юзербота")
+
+if lang == "ru":
+    add_module("ping", __file__)
+    add_command("ping", f"{prefix}ping", "получить пинг юзербота")
+else:
+    add_module("ping", __file__)
+    add_command("ping", f"{prefix}ping", "Get the ping of a userbot.")
