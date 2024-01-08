597c3d842f376e81a97129826f5872b0e49a5df6
Fimkov <123477029+Timka4959000@users.noreply.github.com>
2024-01-08 16:58:05 +0700

    Update ~_###_zzz_logo.py

--- plugins/~_###_zzz_logo.py
+++ plugins/~_###_zzz_logo.py
@@ -1,9 +1,10 @@
 from colorama import Fore, Style
 import os
 from info import version
-
 from helps.get_prefix import get_prefix
+from helps.scripts import get_lang
 
+lang = get_lang()
 prefix = get_prefix()
 
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -12,9 +13,17 @@ print(Fore.LIGHTCYAN_EX + "|_   _|(_) _ __ ___  | | __  __ _ | | | | ___   ___  
 print(Fore.MAGENTA + "  | |  | || '_ ` _ \ | |/ / / _` || | | |/ __| / _ \| '__||  _ \  / _ \ | __|")
 print(Fore.LIGHTCYAN_EX + "  | |  | || | | | | ||   < | (_| || |_| |\__ \|  __/| |   | |_) || (_) || |_ ")
 print(Fore.MAGENTA + "  |_|  |_||_| |_| |_||_|\_\ \__,_| \___/ |___/ \___||_|   |____/  \___/  \__|\n")
-print(Fore.LIGHTCYAN_EX + f"Текущий префикс: [{prefix}]")
-print(Fore.MAGENTA + f"Версия юзербота: {version}\n")
-print(Fore.LIGHTCYAN_EX + f"Для получения помощи - {prefix}help")
-print(Fore.MAGENTA + f"Для получения информации о юзерботе - {prefix}bot")
-print(Fore.LIGHTCYAN_EX + f"Для получения пинга - {prefix}ping")
-print(Style.RESET_ALL)
+if lang == "ru":
+    print(Fore.LIGHTCYAN_EX + f"Текущий префикс: [{prefix}]")
+    print(Fore.MAGENTA + f"Версия юзербота: {version}\n")
+    print(Fore.LIGHTCYAN_EX + f"Для получения помощи - {prefix}help")
+    print(Fore.MAGENTA + f"Для получения информации о юзерботе - {prefix}bot")
+    print(Fore.LIGHTCYAN_EX + f"Для получения пинга - {prefix}ping")
+    print(Style.RESET_ALL)
+else:
+    print(Fore.LIGHTCYAN_EX + f"Current prefix: [{prefix}]")
+    print(Fore.MAGENTA + f"Userbot version: {version}\n")
+    print(Fore.LIGHTCYAN_EX + f"For help - {prefix}help")
+    print(Fore.MAGENTA + f"To get info about the userbot - {prefix}bot")
+    print(Fore.LIGHTCYAN_EX + f"To get ping - {prefix}ping")
+    print(Style.RESET_ALL)
