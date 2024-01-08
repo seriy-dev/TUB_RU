d8ec76694932e5b2dfbdbc00d1af0b180bf077b6
Fimkov <123477029+Timka4959000@users.noreply.github.com>
2024-01-08 16:55:57 +0700

    Update bot.py

--- plugins/bot.py
+++ plugins/bot.py
@@ -5,9 +5,42 @@ import platform
 from platform import python_version
 from info import version
 from helps.modules import add_module, add_command, modules
-
 from helps.get_prefix import get_prefix
+from helps.scripts import get_lang
+
+lang = get_lang()
 prefix = get_prefix()
+
+
+class Texts:
+    @staticmethod
+    def get_texts(me, system, prefix):
+        return {"info": {
+            "ru": f"""
+<b><emoji id='4999015678238262018'>✨</emoji> | TimkaUserBot  
+👤 | Владелец: {me.mention}
+📁 | Кол-во модулей: {len(modules)}
+💻 | Система: {system}
+❗ | Префикс: {prefix}
+🤖 | Версия юзербота: {version}
+🐍 | Python: {python_version()}
+🥧 | Pyrogram: {__version__}
+👑 | Юзербот создан @fimkov</b>
+            """,
+            "en": f"""
+<b><emoji id='4999015678238262018'>✨</emoji> | TimkaUserBot  
+👤 | Owner: {me.mention}
+📁 | Number of modules: {len(modules)}
+💻 | System: {system}
+❗ | Prefix: {prefix}
+🤖 | Userbot version: {version}
+🐍 | Python: {python_version()}
+🥧 | Pyrogram: {__version__}
+👑 | Userbot by @fimkov</b>
+            """
+        }
+        }
+
 
 @Client.on_message(filters.command("bot", prefixes=prefix) & filters.me)
 async def bot(client, message):
@@ -19,32 +52,20 @@ async def bot(client, message):
         system = f"Termux [Android {android_version}]"
     else:
         system = platform.system()
+
     me = await client.get_me()
+    text_versions = Texts.get_texts(me, system, prefix)
+
     try:
-        await client.send_photo(chat_id=message.chat.id, photo='logo.png', caption=f"""
-<b><emoji id='4999015678238262018'>✨</emoji> | TimkaUserBot  
-👤 | Владелец: {me.mention}
-📁 | Кол-во модулей: {len(modules)}
-💻 | Система: {system}
-❗ | Префикс: {prefix}
-🤖 | Версия юзербота: {version}
-🐍 | Python: {python_version()}
-🥧 | Pyrogram: {__version__}
-👑 | Создатель юзербота: @fimkov</b>
-    """)
+        await client.send_photo(chat_id=message.chat.id, photo='logo.png', caption=text_versions["info"][lang])
         await message.delete()
     except:
-        await message.edit(f"""
-<b><emoji id='4999015678238262018'>✨</emoji> | TimkaUserBot
-👤 | Владелец: {me.mention}
-📁 | Кол-во модулей: {len(modules)}
-💻 | Система: {system}
-❗ | Префикс: {prefix}
-🤖 | Версия юзербота: {version}
-🐍 | Python: {python_version()}
-🥧 | Pyrogram: {__version__}
-👑 | Создатель юзербота: @fimkov</b>
-    """)
+        await message.edit(text_versions["info"][lang])
 
-add_module("userbot_info", __file__)
-add_command("userbot_info", f"{prefix}bot", "Получить информацию о юзерботе")
+
+if lang == "ru":
+    add_module("userbot_info", __file__)
+    add_command("userbot_info", f"{prefix}bot", "Получить информацию о юзерботе")
+else:
+    add_module("userbot_info", __file__)
+    add_command("userbot_info", f"{prefix}bot", "Get information about the userbot")
