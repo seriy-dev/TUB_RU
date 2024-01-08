0bdf1b086ff2b7abbe2b34f6616daf9c381d5394
Fimkov <123477029+Timka4959000@users.noreply.github.com>
2024-01-08 16:56:32 +0700

    Update change_prefix.py

--- plugins/change_prefix.py
+++ plugins/change_prefix.py
@@ -1,25 +1,48 @@
 from pyrogram import Client, filters
 from helps.modules import add_module, add_command
 from helps.scripts import set_key, restart
-
+from helps.scripts import get_lang
 from helps.get_prefix import get_prefix
+
+lang = get_lang()
 prefix = get_prefix()
+
+class Texts:
+    @staticmethod
+    def get_texts(new_prefix):
+        return {"error": {
+            "ru": f"Вы неправильно используете команду! Помощь - {prefix}help changeprefix",
+            "en": f"You are using the command incorrectly! Help - {prefix}help changeprefix"
+        },
+            "prefix": {
+                "ru": f"Новый префикс: {new_prefix}",
+                "en": f"New prefix: {new_prefix}"
+            },
+            "reload": {
+                "ru": "<b>Перезагружаю юзербота...</b>",
+                "en": "<b>Rebooting the userbot...</b>"
+            }
+        }
 
 @Client.on_message(filters.command("cp", prefixes=prefix) & filters.me)
 async def change_prefix(client, message):
     try:
         new_prefix = message.text.split()[1]
     except IndexError:
-        await message.reply_text(
-            "Вы неправильно используете команду! Помощь - !help changeprefix"
-        )
+        text_versions = Texts.get_texts(new_prefix)
+        await message.edit(text_versions["error"][lang])
         return
+
+    text_versions = Texts.get_texts(new_prefix)
+
     await set_key("prefix", new_prefix)
-    await message.edit(
-        f"Новый префикс: [{message.text.split()[1]}]"
-    )
-    await message.reply("<b>Перезагружаю юзербота...</b>")
+    await message.edit(text_versions["prefix"[lang]])
+    await message.reply(text_versions['reload'][lang])
     await restart(message=message)
 
-add_module("changeprefix", __file__)
-add_command("cp", f"{prefix}changeprefix [префикс]", "меняет префикс")
+if lang == "ru":
+    add_module("changeprefix", __file__)
+    add_command("changeprefix", f"{prefix}cp [префикс]", "меняет префикс")
+else:
+    add_module("changeprefix", __file__)
+    add_command("changeprefix", f"{prefix}cp [prefix]", "changes prefix")
